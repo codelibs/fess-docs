a3d94bc7d2de18ae481a5b155d90eb14fd19e530
Kaoru FUZITA <kaorufuzita@gmail.com>
2014-11-28 04:19:17 +0900

    Fixed global replacement extension.

--- conf/ext.py
+++ conf/ext.py
@@ -11,6 +11,53 @@ from pygments.lexers.web import PhpLexer
 
 from ConfigParser import SafeConfigParser
 
+def setup(app):
+    # Monkey-patch html.HTMLTranslator.visit_literal_block so that
+    # a node with the "highlight" attribute is always highlighted.
+    old_visit = html.HTMLTranslator.visit_literal_block
+    def new_visit(self, node):
+        if node.get('highlight'):
+            node.rawsource = node.astext()
+        return old_visit(self, node)
+    html.HTMLTranslator.visit_literal_block = new_visit
+
+    directives.register_directive('parsed-code', ParsedCodeBlock)
+
+    # Add "project" to the default substitutions.
+    env.default_substitutions = set(['version', 'release', 'today', 'project'])
+
+    # Additionnal lexer for inline PHP code blocks.
+    lexers['inline-php'] = PhpLexer(startinline=True)
+
+    # import parameters
+    conf = SafeConfigParser()
+    replace = []
+    rst_prolog = ""
+    import os
+    try:
+        # read from build directory
+        #conf.read(os.path.join(os.getcwd(), 'conf.ini'))
+        # read from conf directory
+        conf.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'conf.ini'))
+        for key, value in conf.items('sphinx'):
+            app.config._raw_config[key] = value
+        # replace =  conf.items('replace')
+        for key, value in conf.items('replace'):
+            rst_prolog += "\n.. |" + key + "| replace:: " + value
+    except:
+        print 'Not found conf.ini'
+
+    # includes from each directory
+    rst_prolog += u'''
+.. include:: _defs.rst_
+    '''
+
+    app.config._raw_config["rst_prolog"] = rst_prolog
+
+    # custom styles
+    app.add_stylesheet('custom.css')
+
+
 # Add "parsed-code" directive that combines both the features
 # of "parsed-literal" (support for inline markup) and "sourcecode"
 # (syntax highlighting).
@@ -32,41 +79,4 @@ class ParsedCodeBlock(ParsedLiteral):
         res[0]['linenos'] = 'linenos' in self.options
         res[0]['highlight'] = True
         return res
-
-def setup(app):
-    # Monkey-patch html.HTMLTranslator.visit_literal_block so that
-    # a node with the "highlight" attribute is always highlighted.
-    old_visit = html.HTMLTranslator.visit_literal_block
-    def new_visit(self, node):
-        if node.get('highlight'):
-            node.rawsource = node.astext()
-        return old_visit(self, node)
-    html.HTMLTranslator.visit_literal_block = new_visit
-
-    directives.register_directive('parsed-code', ParsedCodeBlock)
-
-    # Add "project" to the default substitutions.
-    env.default_substitutions = set(['version', 'release', 'today', 'project'])
-
-    # Additionnal lexer for inline PHP code blocks.
-    lexers['inline-php'] = PhpLexer(startinline=True)
-
-    # import parameters
-    conf = SafeConfigParser()
-    #conf.read('conf.ini')
-    import os
-    try:
-        conf.read(os.path.join(os.getcwd(), 'conf.ini'))
-        for key, value in conf.items('sphinx'):
-            exec("%s = %r" % (key, value)) in globals()
-    except:
-        print 'Not found conf.ini'
-
-    # global includes
-    rst_prolog = u'''
-.. include:: _definition.rst_
-    '''
-
-    # custom styles
-    app.add_stylesheet('custom.css')
 
