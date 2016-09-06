de62cb2929e231661c71055f627301abc5987fcf
Kaoru FUZITA <kaorufuzita@gmail.com>
2016-09-06 21:10:34 +0900

    change the theme to sphinx_rtd_theme

--- conf/conf.py
+++ conf/conf.py
@@ -130,9 +130,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
-#html_theme = 'bootstrap'
-html_theme = 'basicstrap'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -142,7 +140,7 @@ html_theme = 'basicstrap'
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 sys.path.append(os.path.abspath('_themes'))
-html_theme_path = ['_themes']
+html_theme_path = ["_themes", ]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -403,74 +401,21 @@ pdf_language = language
 #pdf_default_dpi = 94
 #pdf_default_dpi = 50
 
-# -- HTML theme options for `basicstrap` style -------
+# -- HTML theme options for `sphinx_rtd_theme` style -------
 
 html_theme_options = {
-
-    # Set the lang attribute of the html tag. Defaults to 'en'
-    'lang': language,
-    # Disable showing the sidebar. Defaults to 'false'
-    'nosidebar': False,
-    # Show header searchbox. Defaults to false. works only "nosidber=True",
-    'header_searchbox': False,
-
-    # Put the sidebar on the right side. Defaults to false.
-    'rightsidebar': False,
-    # Set the width of the sidebar. Defaults to 3
-    'sidebar_span': 3,
-
-    # Fix navbar to top of screen. Defaults to true
-    'nav_fixed_top': True,
-    # Fix the width of the sidebar. Defaults to false
-    'nav_fixed': False,
-    # Set the width of the sidebar. Defaults to '900px'
-    'nav_width': '900px',
-    # Fix the width of the content area. Defaults to false
-
-    'content_fixed': False,
-    # Set the width of the content area. Defaults to '900px'
-    'content_width': '900px',
-    # Fix the width of the row. Defaults to false
-    'row_fixed': False,
-
-    # Disable the responsive design. Defaults to false
-    'noresponsive': False,
-    # Disable the responsive footer relbar. Defaults to false
-    'noresponsiverelbar': False,
-    # Disable flat design. Defaults to false.
-    # Works only "bootstrap_version = 3"
-    'noflatdesign': False,
-
-    # Enable Google Web Font. Defaults to false
-    'googlewebfont': False,
-    # Set the URL of Google Web Font's CSS.
-    # Defaults to 'http://fonts.googleapis.com/css?family=Text+Me+One'
-    'googlewebfont_url': 'http://fonts.googleapis.com/css?family=Lily+Script+One',  # NOQA
-    # Set the Style of Google Web Font's CSS.
-    # Defaults to "font-family: 'Text Me One', sans-serif;"
-    'googlewebfont_style': u"font-family: 'Lily Script One' cursive;",
-
-    # Set 'navbar-inverse' attribute to header navbar. Defaults to false.
-    'header_inverse': False,
-    # Set 'navbar-inverse' attribute to relbar navbar. Defaults to false.
-    'relbar_inverse': False,
-
-    # Enable inner theme by Bootswatch. Defaults to false
-    'inner_theme': False,
-    # Set the name of innner theme. Defaults to 'bootswatch-simplex'
-    'inner_theme_name': 'bootswatch-simplex',
-
-    # Select Twitter bootstrap version 2 or 3. Defaults to '3'
-    'bootstrap_version': '3',
-
-    # Show "theme preview" button in header navbar. Defaults to false.
-    'theme_preview': False,
-
-    # Set the Size of Heading text. Defaults to None
-    # 'h1_size': '3.0em',
-    # 'h2_size': '2.6em',
-    # 'h3_size': '2.2em',
-    # 'h4_size': '1.8em',
-    # 'h5_size': '1.4em',
-    # 'h6_size': '1.1em',
+    'collapse_navigation': False,
+    'display_version': False,
+#    'navigation_depth': 3,
 }
+
+html_context = {
+#    "context_path": "",
+    "display_github": True,
+#    "last_updated": True,
+    "commit": False,
+    "github_user": "codelibs",
+    "github_repo": "fess-docs",
+    "github_version": "master",
+}
+
