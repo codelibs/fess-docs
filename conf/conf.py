3e4c9a40ce7197b4a2c66cf821bd44e8749f90ee
Shinsuke Sugaya <shinsuke@apache.org>
2020-02-04 22:44:38 +0900

    2020

--- conf/conf.py
+++ conf/conf.py
@@ -58,7 +58,7 @@ project = u'Fess'
 if 'SPHINX_PROJECT' in os.environ:
     project = os.environ['SPHINX_PROJECT']
 
-copyright = u'2019 CodeLibs Project'
+copyright = u'2020 CodeLibs Project'
 
 # Document title
 title = 'Fess'
